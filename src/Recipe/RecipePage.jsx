974f62452e9f109c0716875ff528b9685f6d050b
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-02-04 22:47:35 -0600

    React Practice

---
+++ src/Recipe/RecipePage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function RecipePage() {
+    return (
+        <>
+        <Doctype html />
+        <html>
+            <head>
+                <title>The Odin Recipes</title>
+            </head>
+            <body>
+                <h1>Recipes</h1>
+            </body>
+        </html>
+        </>
+    );
+}
+
+export default RecipePage;
+
